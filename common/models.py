80da11911e931587a58c0b93b0699191251bbcc8
Filip Rybka <filip.rybka@venturedevs.com>
2019-07-03 14:04:24 +0200

    Boolean in models

--- common/models.py
+++ common/models.py
@@ -1,18 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
-COMPLETE_CHOICES = [
-    ('True', 'Completed'),
-    ('False', 'Uncompleted'),
-]
-
 
 class ToDoList(models.Model):
-    COMPLETED = 'True'
-    UNCOMPLETED = 'False'
-    COMPLETE_CHOICES = [
-        (COMPLETED, 'Completed'), (UNCOMPLETED, 'Uncompleted')
-    ]
-    completed = models.CharField(max_length=2, choices=COMPLETE_CHOICES, default=UNCOMPLETED,)
+    completed = models.BooleanField(default=False)
     task = models.CharField(max_length=250)
     due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
